c5491f43ef4532664fb451c5a676760dd1093680
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-19 18:55:34 -0400

    Edit the Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,6 @@ import React from "react";
 
 function Search({ search, onHandleSearch }) {
 
-  // Handle the change event on the search input field and pass the search value to the PlantPage component
-  const handleChange = (e) => {
-    onHandleSearch(e);
-  }
-
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -15,7 +10,7 @@ function Search({ search, onHandleSearch }) {
         id="search"
         placeholder="Type a name to search..."
         value={search}
-        onChange={(e) => handleChange(e)}
+        onChange={(e) => onHandleSearch(e)}
       />
     </div>
   );
